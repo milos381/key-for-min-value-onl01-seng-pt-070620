bca4413929e7b84ac22a89b57c249fdbc4a5dd22
Milos Popovic <milos_popovic@outlook.com>
2020-07-12 18:18:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     binding.pry
     if value < lowest_value || lowest_value = nil#if value is closest to lowest_value
     #return key asso with that value
