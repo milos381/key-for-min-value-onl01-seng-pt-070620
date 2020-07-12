e7147090bcae3af286fecfcd31aac59534dda96c
Milos Popovic <milos_popovic@outlook.com>
2020-07-12 18:59:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,12 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
 
     if lowest_value == nil || value < lowest_value
+      #this line up explanation:
+      #first we set up the lowest_value to nil 
+      #first iteration happens and now value is 500
+      #since it is 500, lowest_value is now 500 nad not nil anymore
+      #after second iteration, value becomes 2
+      #since 2 < 500, lowest_value now is reseted to 2
       lowest_value = value
       lowest_key = key
     end
