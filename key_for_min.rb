993685c694c19846cf9584e54cfdad016419c822
Milos Popovic <milos_popovic@outlook.com>
2020-07-12 17:32:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.collect do |key, value|
+    if value == value
+      0
+    elsif value < value
+      -1
+    elsif value > value
+      1
+    end
+  end
 end
